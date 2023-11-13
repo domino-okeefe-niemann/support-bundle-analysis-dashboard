c9aa544283947c60092d67a5ac3a42ad25c79209
Okeefe Niemann <okeefe.niemann@dominodatalab.com>
2023-11-13 08:03:26 -0800

    Pushed from Domino: https://prod-field.cs.domino.tech/workspace/okeefe_niemann/allstate_log_github?executionId=653080339a52bf4b7afe35fc

---
+++ machine_learning/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("/mnt/code/machine_learning/")
+
+import log_error_classifier
+
+model_path = '/mnt/artifacts/models/log_classification_20231019_205618'
+classifier = log_error_classifier.HuggingFaceClassifier(path_or_pretrained=model_path)
+
+
+#my_model = 
+
+def predict(text):
+    predictions, probabilities = classifier.predict(text)
+    prediction_strings = [classifier.label_to_error[pred] for pred in predictions]
+    print(prediction_strings)
+
+    return dict(error_class=prediction_strings)
+
+if __name__ == "__main__":
+    tst_string = "This is a test"
+    text = [tst_string for i in range(0, 2)]
+    #text = ["This is a sentence. Does it have an error?", "There are many more to go"]
+    predict(text)
+    print("Did I make it here?")
